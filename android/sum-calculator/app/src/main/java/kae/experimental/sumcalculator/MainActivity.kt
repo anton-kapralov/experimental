0164a4a2453a6802d7e75d18a58fc6d3a62e1021
Anton Kapralov <a.e.kapralov@gmail.com>
2021-04-11 20:55:17 -0700

    Update the main view and implement calculation logic.

--- android/sum-calculator/app/src/main/java/kae/experimental/sumcalculator/MainActivity.kt
+++ android/sum-calculator/app/src/main/java/kae/experimental/sumcalculator/MainActivity.kt
@@ -1,11 +1,21 @@
 package kae.experimental.sumcalculator
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.EditText
+import androidx.appcompat.app.AppCompatActivity
+import java.lang.Integer.parseInt
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    fun onCalculate(view: View) {
+        val a = parseInt(findViewById<EditText>(R.id.a).text.toString())
+        val b = parseInt(findViewById<EditText>(R.id.b).text.toString())
+        val c = a + b
+        findViewById<EditText>(R.id.c).setText(c.toString())
     }
 }
