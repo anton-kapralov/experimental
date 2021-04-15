e262251524b320da3393f007c2e11bf8d73721cb
Anton Kapralov <a.e.kapralov@gmail.com>
2021-04-14 22:10:25 -0700

    Use ReverseRunes in helloworld.go.

--- golang/helloworld/helloworld.go
+++ golang/helloworld/helloworld.go
@@ -1,7 +1,10 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"github.com/anton-kapralov/experimental/golang/morestrings"
+)
 
 func main() {
-  fmt.Println("Hello, World!")
+	fmt.Println(morestrings.ReverseRunes("Hello, World!"))
 }
