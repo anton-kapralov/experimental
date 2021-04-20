31a5d7cb79e10c4bd3fc997446067b6b32f4ae61
Anton Kapralov <a.e.kapralov@gmail.com>
2021-04-20 14:52:58 -0700

    Generate HelloWorld project base for Calculator iOS app.

---
+++ ios/calculator/calculator/ContentView.swift
@@ -0,0 +1,22 @@
+//
+//  ContentView.swift
+//  calculator
+//
+//  Created by Anton Kapralov on 18.04.2021.
+//
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ ios/calculator/calculator/calculatorApp.swift
@@ -0,0 +1,18 @@
+//
+//  calculatorApp.swift
+//  calculator
+//
+//  Created by Anton Kapralov on 18.04.2021.
+//
+//
+
+import SwiftUI
+
+@main
+struct calculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
