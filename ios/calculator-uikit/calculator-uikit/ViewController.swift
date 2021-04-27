7745773397374b8cea0bf7cf7a1972513c0be6fa
Anton Kapralov <a.e.kapralov@gmail.com>
2021-04-26 20:25:04 -0700

    Add UI and the logic for calculating the sum of two numbers.

--- ios/calculator-uikit/calculator-uikit/ViewController.swift
+++ ios/calculator-uikit/calculator-uikit/ViewController.swift
@@ -9,10 +9,21 @@ import UIKit
 
 class ViewController: UIViewController {
 
+  @IBOutlet weak var leftTextField: UITextField!
+  @IBOutlet weak var rightTextField: UITextField!
+  @IBOutlet weak var sumTextField: UITextField!
+  
   override func viewDidLoad() {
     super.viewDidLoad()
     // Do any additional setup after loading the view.
   }
+  
+  @IBAction func onCalculate(_ sender: Any) {
+    let left: Int = Int(leftTextField.text ?? "") ?? 0
+    let right: Int = Int(rightTextField.text ?? "") ?? 0
 
+    sumTextField.text = String(left + right)
+  }
+  
 }
 
