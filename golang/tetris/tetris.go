8b74cc3280bce927f41524fa870c140c24d9d795
Anton Kapralov <a.e.kapralov@gmail.com>
2022-12-06 21:24:24 -0800

    [tetris] Implement moving down and sticking

--- golang/tetris/tetris.go
+++ golang/tetris/tetris.go
@@ -4,11 +4,24 @@ import (
 	"fmt"
 	"github.com/anton-kapralov/experimental/golang/console"
 	"log"
+	"sync"
+	"time"
 )
 
 const (
 	height = 20
 	width  = 10
+)
+
+type command int
+
+const (
+	commandUndefined command = iota
+	commandUp
+	commandDown
+	commandRight
+	commandLeft
+	commandDrop
 )
 
 type tetrisBoard [height][width]int
@@ -21,10 +34,31 @@ func (t *tetromino) move(dr, dc int) {
 	}
 }
 
+func (t *tetromino) rotate() {
+	//TODO: implement me.
+}
+
 var (
-	board   tetrisBoard
-	current tetromino
+	board    tetrisBoard
+	current  tetromino
+	mu       sync.Mutex
+	gameOver = make(chan struct{})
 )
+
+func getSnapshot() tetrisBoard {
+	mu.Lock()
+	defer mu.Unlock()
+	var snapshot tetrisBoard
+	for i, row := range board {
+		for j, v := range row {
+			snapshot[i][j] = v
+		}
+	}
+	for _, tile := range current {
+		snapshot[tile[0]][tile[1]] = 1
+	}
+	return snapshot
+}
 
 func main() {
 	c, err := console.New()
@@ -34,12 +68,20 @@ func main() {
 	defer c.Close()
 	c.Clear()
 
-	nextTetromino()
+	commands := make(chan command)
 
-inputLoop:
+	nextTetromino()
+	draw(c)
+
+	go gameLoop(commands)
+	go handleCommands(commands, func() { draw(c) })
+	go inputLoop(c, commands)
+
+	<-gameOver
+}
+
+func inputLoop(c *console.Console, commands chan command) {
 	for {
-		c.MoveCursor(0, 0)
-		draw()
 		code, err := c.Read()
 		if err != nil {
 			log.Fatal(err)
@@ -47,25 +89,27 @@ inputLoop:
 		switch code {
 		case console.Up:
 			fmt.Printf("⬆️\r\n")
+			commands <- commandUp
 		case console.Down:
 			fmt.Printf("⬇️\r\n")
+			commands <- commandDrop
 		case console.Right:
 			fmt.Printf("➡️\r\n")
+			commands <- commandRight
 		case console.Left:
 			fmt.Printf("⬅️\r\n")
+			commands <- commandLeft
 		case console.Sigint:
-			break inputLoop
+			gameOver <- struct{}{}
+			return
 		default:
 			fmt.Printf("%x\r\n", code)
 		}
 	}
 }
 
-func nextTetromino() {
-	newTShapeTetromino()
-}
-
-func draw() {
+func draw(c *console.Console) {
+	c.MoveCursor(0, 0)
 	fmt.Print(" ")
 	for i := 0; i < width; i++ {
 		fmt.Print("_")
@@ -90,17 +134,11 @@ func draw() {
 	}
 }
 
-func getSnapshot() tetrisBoard {
-	var snapshot tetrisBoard
-	for i, row := range board {
-		for j, v := range row {
-			snapshot[i][j] = v
-		}
+func nextTetromino() {
+	newTShapeTetromino()
+	if hasCollisions() {
+		gameOver <- struct{}{}
 	}
-	for _, tile := range current {
-		snapshot[tile[0]][tile[1]] = 1
-	}
-	return snapshot
 }
 
 func newTShapeTetromino() {
@@ -114,4 +152,77 @@ func newTShapeTetromino() {
 	current[3][1] = 2
 
 	current.move(0, 4)
+}
+
+func gameLoop(commands chan<- command) {
+	tick := time.Tick(1 * time.Second)
+	for {
+		<-tick
+		commands <- commandDown
+	}
+}
+
+func handleCommands(commands <-chan command, onStateChanged func()) {
+	for {
+		c := <-commands
+		if c == commandUndefined {
+			return
+		}
+		move(c)
+		onStateChanged()
+	}
+}
+
+func move(c command) {
+	mu.Lock()
+	defer mu.Unlock()
+	switch c {
+	case commandUp:
+		rotate()
+	case commandDown:
+		moveDown()
+	case commandDrop:
+		drop()
+	}
+}
+
+func rotate() {
+	current.rotate()
+}
+
+func moveDown() {
+	current.move(1, 0)
+	if hasCollisions() {
+		current.move(-1, 0)
+		stickCurrent()
+		nextTetromino()
+	}
+}
+
+func drop() {
+	for !hasCollisions() {
+		current.move(1, 0)
+	}
+	current.move(-1, 0)
+	stickCurrent()
+	nextTetromino()
+}
+
+func hasCollisions() bool {
+	for _, tile := range current {
+		r := tile[0]
+		c := tile[1]
+		if r < 0 || r >= height || c < 0 || c >= width || board[r][c] != 0 {
+			return true
+		}
+	}
+	return false
+}
+
+func stickCurrent() {
+	for _, tile := range current {
+		r := tile[0]
+		c := tile[1]
+		board[r][c] = 1
+	}
 }
